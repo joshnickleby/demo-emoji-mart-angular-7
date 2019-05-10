8eef49a63c682c4deeba0643ea8f23e7a0b96d2f
joshnickleby <josh.nickleby@astontech.com>
2019-05-10 12:50:43 -0500

    Trying to get emoji-store to work on Angular 7 project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PickerModule} from '@ctrl/ngx-emoji-mart';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
